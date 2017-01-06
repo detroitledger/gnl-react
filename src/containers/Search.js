424f8b8c5172dadff40cb39d811d5d1c33246ecd
jessicamcinchak <jessica.mcinchak@gmail.com>
2017-01-05 20:42:02 -0500

    start refactoring search component

--- src/containers/Search.js
+++ src/containers/Search.js
@@ -1,13 +1,57 @@
-import React from 'react';
-import { Row, Col } from 'react-bootstrap';
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { graphql, compose } from 'react-apollo';
+import gql from 'graphql-tag';
+import update from 'immutability-helper';
 
-export default function Search() {
-  return (
-    <Row>
-      <Col md={6}>
-      	<h2>Search organizations</h2>
-      	<input type="text" placeholder="Search by EIN" />
-      </Col>
-    </Row>
-  );
+/** TODO store ein search value as state, pass to query, return results - and learn more about compose */
+
+class Search extends React.Component {
+  
+  constructor(args) {
+    super(args);
+  }
+
+  handleChange(event) {
+  	console.log({value: event.target.value});
+  }
+
+  render() {
+    return (
+        <div>
+          <h3>Search organizations</h3>
+          <input type='text' 
+            placeholder='Search by EIN' 
+            onChange = {this.handleChange} />
+          <br />
+          <h4>Results: </h4>
+        </div>
+    );
+  }
+
 }
+
+Search.propTypes = {
+  data: PropTypes.shape({
+    irsOrganization: PropTypes.object,
+  }).isRequired,
+};
+
+const EIN_QUERY = gql`
+query getEIN($ein: String!) {
+  irsOrganization(ein: $ein) {
+    ein
+  }
+}
+`;
+
+export default compose(
+  graphql(EIN_QUERY, {
+    options: ({ params }) => ({
+      variables: { ein: params.search_value },
+    }),
+    props({ data: { irsOrganization } }) {
+      return { data: { irsOrganization } };
+    },
+  }),
+)(Search);
