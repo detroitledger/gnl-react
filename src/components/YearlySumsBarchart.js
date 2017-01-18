3d0c74d79c6c1f01043800361c5d47b7a73d0927
Benjamin Chodoroff <bc@thermitic.net>
2017-01-18 11:04:03 -0500

    working summary chart for grants received & funded

--- src/components/YearlySumsBarchart.js
+++ src/components/YearlySumsBarchart.js
@@ -1,7 +1,21 @@
 import React, { PropTypes } from 'react';
+import { BarChart } from 'react-svg-chart';
+import numeral from 'numeral';
 
-const YearlySumsBarchart = (sums) => {
-  return (<p>sums chart todo here</p>);
+const YearlySumsBarchart = ({ sums }) => {
+  const data = Object.keys(sums).map((k, i) => ({
+    label: k,
+    value: sums[k],
+  }));
+
+  return (
+    <BarChart
+      bars={data}
+      height={ 400 }
+      width={ 600 }
+      formatValue={v => `$${numeral(v).format('0,0[.]00')}`}
+    />
+  );
 };
 
 YearlySumsBarchart.propTypes = {
