4b7e54808f2f7859326bff6e6993f63cc964eb1e
Benjamin Chodoroff <bc@thermitic.net>
2017-01-28 16:46:16 -0500

    use nicer syntax for this speical object prop

--- src/network/create-apollo-client.js
+++ src/network/create-apollo-client.js
@@ -3,8 +3,8 @@ import ApolloClient, { addTypename } from 'apollo-client';
 export default options => new ApolloClient(Object.assign({}, {
   queryTransformer: addTypename,
   dataIdFromObject: (result) => {
-    if (result.id && result.__typename) { // eslint-disable-line no-underscore-dangle
-      return result.__typename + result.id; // eslint-disable-line no-underscore-dangle
+    if (result.id && result['__typename']) {
+      return result['__typename'] + result.id;
     }
     return null;
   },
