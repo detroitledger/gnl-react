3cc6e244f1b44016ce528b55343d8dcc97e36083
Matt Hampel <mahatm@gmail.com>
2019-01-05 12:54:46 -0500

    Tidy up footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,17 +2,33 @@ import React from 'react';
 
 export default () => (
   <footer>
-    <form action="https://detroitledger.us7.list-manage.com/subscribe/post?u=5f6c82c52e266389140a08ab5&amp;id=ffe8bfd2f0" method="post" name="mc-embedded-subscribe-form" target="_blank">
-      Subscribe for news and updates on Detroit nonprofits:
-      <input placeholder="Your email" type="email" name="EMAIL" />
-      <input type="submit" value="Subscribe" name="subscribe" />
-      <div style={{position: 'absolute', left: '-5000px'}}>
-        <input type="text" name="b_5f6c82c52e266389140a08ab5_ffe8bfd2f0" tabIndex="-1" readOnly value="" />
-      </div>
-    </form>
+    <div class="container">
+      <form
+        action="https://detroitledger.us7.list-manage.com/subscribe/post?u=5f6c82c52e266389140a08ab5&amp;id=ffe8bfd2f0"
+        method="post"
+        name="mc-embedded-subscribe-form"
+        target="_blank"
+      >
+        Subscribe for news and updates on Detroit nonprofits:
+        <input placeholder="Your email" type="email" name="EMAIL" />
+        <input type="submit" value="Subscribe" name="subscribe" />
+        <div style={{ position: 'absolute', left: '-5000px' }}>
+          <input
+            type="text"
+            name="b_5f6c82c52e266389140a08ab5_ffe8bfd2f0"
+            tabIndex="-1"
+            readOnly
+            value=""
+          />
+        </div>
+      </form>
 
-    <p>The Ledger is at 2727 2nd Avenue, Suite 200, Detroit MI.</p>
-
-    <p>This data is made available under the <a href="http://opendatacommons.org/licenses/odbl/1.0/">Open Database License</a>. Any rights in individual contents of the database are licensed under the <a href="http://opendatacommons.org/licenses/dbcl/1.0/">Database Contents License.</a></p>
+      <p>
+        This data is made available under the{' '}
+        <a href="http://opendatacommons.org/licenses/odbl/1.0/">Open Database License</a>. Any
+        rights in individual contents of the database are licensed under the{' '}
+        <a href="http://opendatacommons.org/licenses/dbcl/1.0/">Database Contents License.</a>
+      </p>
+    </div>
   </footer>
-)
+);
