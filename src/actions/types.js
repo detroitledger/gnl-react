59a04c06bd30c30d845fcdccabfa4b41f6c95378
Benjamin Chodoroff <bc@thermitic.net>
2019-09-26 08:15:57 -0400

    use google oauth - replace one mess w/ another :):)

--- src/actions/types.js
+++ src/actions/types.js
@@ -4,3 +4,9 @@ export const DISMISS_ERROR = 'DISMISS_ERROR';
 export const FORM_SET_CSRF_TOKEN = 'FORM_SET_CSRF_TOKEN';
 
 export const UI_SET_GRANT_VIEW = 'UI_SET_GRANT_VIEW';
+
+export const AUTH_REQUEST = 'AUTH_REQUEST';
+export const AUTH_SUCCESS = 'AUTH_SUCCESS';
+export const AUTH_FAILURE = 'AUTH_FAILURE';
+export const AUTH_SET_ID_TOKEN = 'AUTH_SET_ID_TOKEN';
+export const AUTH_LOGOUT = 'AUTH_LOGOUT';
