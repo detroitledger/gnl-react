1c7c22c129b994d2a98033f04cf6a59fab4f91e6
jessica <jessica.mcinchak@gmail.com>
2020-04-05 17:34:54 +0200

    basic grants page layout, add links from GrantTable

--- src/components/GrantRow.js
+++ src/components/GrantRow.js
@@ -3,11 +3,15 @@ import numeral from 'numeral';
 
 import { slugify } from '../utils';
 
-const GrantRow = ({ amount, description, org, orgUuid, summary, years }) => {
+const GrantRow = ({ amount, description, org, orgUuid, summary, years, uuid }) => {
   let name = description;
+
   if (summary) {
-    name = <a href={`/organizations/${slugify(name)}/${orgUuid}`}>{org}</a>;
+    name = <strong><a href={`/organizations/${slugify(name)}/${orgUuid}`}>{org}</a></strong>;
+  } else if (uuid) {
+    name = <span>{name} <a href={`/grants/${uuid}`}>>></a></span>;
   }
+
   return (
     <tr className={summary ? 'summary' : ''}>
       <td>{name}</td>
--- src/containers/Grants.js
+++ src/containers/Grants.js
@@ -5,6 +5,17 @@ import { Switch, Route, useRouteMatch, useParams } from 'react-router-dom';
 import { gql } from 'apollo-boost';
 import { useQuery } from '@apollo/react-hooks';
 
+import Helmet from 'react-helmet';
+
+import { Col, Row } from 'react-bootstrap';
+
+import moment from 'moment';
+
+import { slugify } from '../utils';
+
+import Page from '../components/Page';
+import Flag from '../components/Flag';
+
 const GET_GRANT = gql`
   query grant($grantId: String!) {
     grant(uuid: $grantId) {
@@ -54,10 +65,62 @@ const Grant = () => {
   if (loading) return 'Loading...';
   if (error) return `Error! ${error}`;
 
+  if (!data.grant) return `Oof!`;
+
+  const { to, from, source } = data.grant;
+  const { dateFrom, dateTo, amount, description } = cleanse(data.grant);
+
   return (
-    <div>
-      <h3>Requested grant ID: {grantId}</h3>
-      <pre>{JSON.stringify(data)}</pre>
-    </div>
+    <Page>
+      <Helmet title={grantId} />
+      <h1>{amount}</h1>
+
+      <Row>
+        <Col md={6}>
+          From
+          <h2>
+            <a href={`/organizations/${slugify(from.name)}/${from.uuid}`}>{from.name}</a>
+          </h2>
+        </Col>
+        <Col md={6}>
+          To 
+          <h2>
+            <a href={`/organizations/${slugify(to.name)}/${to.uuid}`}>{to.name}</a>
+          </h2>
+        </Col>
+      </Row>
+      <p>From {dateFrom} to {dateTo}</p>
+
+      <p>{description}</p>
+      <p>Source {source}</p>
+      <Flag />
+
+      <p>**related grants table tbd**</p>
+    </Page>
   );
+};
+
+const cleanse = (grant) => {
+  const dateFrom = moment(
+    grant.dateFrom,
+    'ddd, DD MMM YYYY HH:mm:ss ZZ'
+  ).year();
+
+  const dateTo = moment(
+    grant.dateTo,
+    'ddd, DD MMM YYYY HH:mm:ss ZZ'
+  ).year();
+
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0,
+  });
+
+  return {
+    dateFrom, 
+    dateTo,
+    description: grant.description || 'No description available.',
+    amount: formatter.format(parseInt(grant.amount, 10)),
+  }
 };
